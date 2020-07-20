9aa743c245e45ac1cc21a4d5c15775e91cf97df1
Oh judam <dhwnkea@naver.com>
2020-07-20 17:15:11 +0900

    3의배수 배열이용 출력

--- src/practice01/Prob01.java
+++ src/practice01/Prob01.java
@@ -6,7 +6,12 @@ public class Prob01 {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner( System.in );
-		
+		int number = Integer.parseInt(scanner.nextLine());
+		if(number %3==0) {
+			System.out.println("3의 배수입니다.");
+		}else {
+			System.out.println("3의 배수가 아닙니다.");
+		}
 		/* 코드를 작성합니다. */
 		
 		scanner.close();
--- src/practice01/Prob02.java
+++ src/practice01/Prob02.java
@@ -4,6 +4,19 @@ public class Prob02 {
 
 	public static void main(String[] args) {
 		/* 코드를 작성합니다. */
+	
+		int[] nums = new int[10];
+	
+		for(int j=0;j<10;j++) {
+			
+			for(int i=0;i<10;i++) {
+				nums[i] = i+j+1;
+				System.out.print(nums[i]+" ");
+			}
+			System.out.println();
+		
+		}
+		
 	}
 
 }
